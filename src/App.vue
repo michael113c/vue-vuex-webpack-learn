42fa848f743d0a3876c48304fbf3ec7682f936cb
hehongwei44@gmail.com <252377352a>
2017-06-26 18:36:22 +0800

    增加项目细节说明

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-	<div>
-    <p>efg</p>
-		<transition name="router-fade" mode="out-in">
-			<keep-alive>
-			    <router-view v-if="$route.meta.keepAlive"></router-view>
-			</keep-alive>
-    	</transition>
-    	<transition name="router-fade" mode="out-in">
-			<router-view v-if="!$route.meta.keepAlive"></router-view>
-		</transition>
-    </div>
+  <div>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+    </transition>
+    <transition name="router-fade" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
 
-  	export default {
-
-  	}
+  export default {}
 
 </script>
 
 <style lang="scss">
-  	@import './style/common';
-	.router-fade-enter-active, .router-fade-leave-active {
-	  	transition: opacity .3s;
-	}
-	.router-fade-enter, .router-fade-leave-active {
-	  	opacity: 0;
-	}
+  @import './style/common';
+
+  .router-fade-enter-active, .router-fade-leave-active {
+    transition: opacity .3s;
+  }
+
+  .router-fade-enter, .router-fade-leave-active {
+    opacity: 0;
+  }
 </style>
