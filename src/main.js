fccabce33e972297015c70852a62e18fa81ac5e7
hehongwei44@gmail.com <252377352a>
2017-06-26 18:13:31 +0800

    增加项目细节说明

--- src/main.js
+++ src/main.js
@@ -1,37 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './router/router'
-import store from './store/'
-import {routerMode} from './config/env'
-import './config/rem'
-import FastClick from 'fastclick'
+import Vue from "vue";
+import VueRouter from "vue-router"; // 在node_modules,工具默认会查找
+import routes from "./router/router"; // 路径以main.js为基准
+import store from "./store/"; // 不指定文件的话，默认加载index.js
+import {routerMode} from "./config/env";
+import FastClick from "fastclick";  // fastclick符合AMD规范
+import "./config/rem";
 
+// 解决移动端单击延迟的问题
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
-    }, false);
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body);
+  }, false);
 }
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
 const router = new VueRouter({
-	routes,
-	mode: routerMode,
-	strict: process.env.NODE_ENV !== 'production',
-	scrollBehavior (to, from, savedPosition) {
-	    if (savedPosition) {
-		    return savedPosition
-		} else {
-			if (from.meta.keepAlive) {
-				from.meta.savedPosition = document.body.scrollTop;
-			}
-		    return { x: 0, y: to.meta.savedPosition ||0}
-		}
-	}
-})
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {  // 如果这个页面被缓存，则记录这个页面的滚动条距离顶部的位置
+        from.meta.savedPosition = document.body.scrollTop;
+      }
+      return {x: 0, y: to.meta.savedPosition || 0}
+    }
+  }
+});
 
 
 new Vue({
-	router,
-	store,
-}).$mount('#app')
+  router,
+  store,
+}).$mount('#app');
 
